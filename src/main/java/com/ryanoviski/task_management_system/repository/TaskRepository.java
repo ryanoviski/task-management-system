650a2845e6c9c1a65ba914b15de2c59b9272bca1
Ryan dos Santos <ryanoviski@gmail.com>
2025-09-08 13:08:50 -0300

    feat: create TaskRepository interface extending JpaRepository

--- src/main/java/com/ryanoviski/task_management_system/repository/TaskRepository.java
+++ src/main/java/com/ryanoviski/task_management_system/repository/TaskRepository.java
@@ -1,5 +1,9 @@
 package com.ryanoviski.task_management_system.repository;
 
-public interface TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ryanoviski.task_management_system.model.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long>{
 
 }
